e20abd47d820060c902e7929335bd5e2b184ba8c
Yodelyoda <47818799+Yodelyoda@users.noreply.github.com>
2022-02-23 12:52:57 +0200

    Add files via upload

---
+++ 2.2lab 1.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _2._2lab_1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            Console.WriteLine("x = ");
+            int x = Convert.ToInt32(Console.Read());
+            Console.WriteLine("y = ");
+            int y = Convert.ToInt32(Console.Read());
+            // розгалуження в повній формі
+            if ((y <= Math.Pow(-x, 2) + 2 && y <= x && y > 0) ||
+                (y <= Math.Pow(-x, 2) + 2 && y >= x && y <= 0))
+                Console.WriteLine("yes");
+            else
+                Console.WriteLine("no");
+            Console.ReadKey();
+        }
+    }
+}
